1fa81bf3d56e697b18ab966659b35c88e8d43881
Francisco Ferreira Serra <fferrese@usersad.everis.int>
2019-01-07 20:38:28 +0000

    views + routing

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,16 @@ router.get('/cart', function(req, res, next) {
   res.render('cart', {script: '/javascripts/cart.js'});
 });
 
+router.get('/checkout', function(req, res, next) {
+  res.render('checkout');
+});
 
+router.get('/catalog', function(req, res, next) {
+  res.render('catalog');
+});
 
+router.get('/product', function(req, res, next) {
+  res.render('product');
+});
 
 module.exports = router;
