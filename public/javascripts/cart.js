2201f2fd31a37565ee6ba0e36bd2ab78b822c9c1
Francisco Ferreira Serra <fferrese@usersad.everis.int>
2019-01-13 22:18:28 +0000

    final fixes

--- public/javascripts/cart.js
+++ public/javascripts/cart.js
@@ -5,7 +5,7 @@ window.onload = function(){
         var index = i + 1
         $('#cartRow').append("<tr><td id=\"Index\">" + index + "</td><td>" + cart[i].name +"</td><td>" + cart[i].price + "</td><td>" + cart[i].qty + "</td><td>" + cart[i].color + "</td><td><button class=\"btn\" onClick=\" deleteItem(" + index + ")\"> <i class=\"fa fa-trash\"></i></button></td></tr>")
         
-        total = total + parseInt(cart[i].price.split("€")[0])
+        total = total + (parseInt(cart[i].price.split("€")[0]) * parseInt(cart[i].qty));
     }
     $('#cartRow').append("<tr><td>total</td><td></td><td><b>"+ total +"€ </b></td></tr>");
 }
@@ -20,3 +20,8 @@ var deleteItem = function(id) {
         location.reload();
     };
 
+var clearCart = function() {
+    window.localStorage.removeItem("shoppingCart");
+    location.reload();
+
+}
