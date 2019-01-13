02ea1c0313fe30cc48a7f982990c7d8ddb54b230
Francisco Ferreira Serra <fferrese@usersad.everis.int>
2019-01-13 01:03:35 +0000

    product qty

--- public/javascripts/product.js
+++ public/javascripts/product.js
@@ -1,3 +1,14 @@
 $('.dropdown-menu li').on('click', function() {
     $('#size').html($(this).html());
 }); 
+
+$('.btnMinus').on('click', function(){
+    $('.qtyInput').val(parseInt($('.qtyInput').val()) - 1).change();
+    if(parseInt($('.qtyInput').val()) < parseInt($('.qtyInput').attr("min")))
+        $('.qtyInput').val(1).change();
+});
+
+
+$('.btnPlus').on('click', function(){
+    $('.qtyInput').val(parseInt($('.qtyInput').val()) + 1).change();
+});
